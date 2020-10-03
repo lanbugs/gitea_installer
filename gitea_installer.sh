b96ca32c08638c4c5dc57e5675ba6cc44dafc312
Maximilian Thoma <admin@lanbugs.de>
2020-10-03 20:25:35 +0200

    add license information to script

--- gitea_installer.sh
+++ gitea_installer.sh
@@ -4,8 +4,20 @@
 # GITEA Installer with Nginx, MariaDB, UFW & Letsencrypt
 # Version 0.1
 # Written by Maximilian Thoma 2020
-# https://lanbugs.de
+# Visit https://lanbugs.de for further informations.
 # -----------------------------------------------------------------------------
+# gitea_installer.sh is free software;  you can redistribute it and/or
+# modify it under the  terms of the  GNU General Public License  as
+# published by the Free Software Foundation in version 2.
+# gitea_installer.sh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; with-out even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the  GNU General Public License for more details.
+# You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+#
 
 LETSENCRYPT='false'
 UFW='false'
